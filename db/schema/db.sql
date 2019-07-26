ee9355988e503b76889e6811583bc937438f1e72
Nelly31 <nelly_main@hotmail.com>
2019-07-26 21:05:19 +0000

    set up database schema

---
+++ db/schema/db.sql
@@ -0,0 +1,35 @@
+-- Drop and recreate Users table (Example)
+
+DROP TABLE IF EXISTS events CASCADE;
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  attendee_id INTEGER references attendees(id)
+);
+
+DROP TABLE IF EXISTS attendees CASCADE;
+CREATE TABLE attendees (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email TEXT,
+);
+
+DROP TABLE IF EXISTS date_times CASCADE;
+CREATE TABLE date_times (
+ id SERIAL PRIMARY KEY NOT NULL,
+ start_date_time,
+ end_date_time,
+);
+
+DROP TABLE IF EXISTS event_attendees CASCADE;
+CREATE TABLE event_attendees (
+  event_id INTEGER references events(id),
+  attendee_id INTEGER references attendees(id)
+);
+
+DROP TABLE IF EXISTS attendee_date_times CASCADE;
+CREATE TABLE attendee_date_times (
+  date_time_id INTEGER references date_times(id),
+  attendee_id INTEGER references attendees(id)
+);
---
+++ db/seeds/db_seeds.sql
@@ -0,0 +1,4 @@
+-- Widgets table seeds here (Example)
+INSERT INTO attendees (name, email) VALUES ('Bob Martin', 'bob.martin@gmail.com');
+INSERT INTO attendees (name, email) VALUES ('Sally Francis', 'sally.francis@hotmail.com');
+
