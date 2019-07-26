8fce2b9f067f540de7bd6670066d96da27230fe7
spiritxhx <spiritxhx@gmail.com>
2019-07-26 17:24:26 -0400

    update the order of the database

--- db/schema/db.sql
+++ db/schema/db.sql
@@ -1,4 +1,17 @@
 -- Drop and recreate Users table (Example)
+DROP TABLE IF EXISTS date_times CASCADE;
+CREATE TABLE date_times (
+ id SERIAL PRIMARY KEY NOT NULL,
+ start_date_time TIMESTAMP,
+ end_date_time TIMESTAMP
+);
+
+DROP TABLE IF EXISTS attendees CASCADE;
+CREATE TABLE attendees (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email TEXT
+);
 
 DROP TABLE IF EXISTS events CASCADE;
 CREATE TABLE events (
@@ -8,28 +21,14 @@ CREATE TABLE events (
   attendee_id INTEGER references attendees(id)
 );
 
-DROP TABLE IF EXISTS attendees CASCADE;
-CREATE TABLE attendees (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email TEXT,
-);
-
-DROP TABLE IF EXISTS date_times CASCADE;
-CREATE TABLE date_times (
- id SERIAL PRIMARY KEY NOT NULL,
- start_date_time,
- end_date_time,
+DROP TABLE IF EXISTS attendee_date_times CASCADE;
+CREATE TABLE attendee_date_times (
+  date_time_id INTEGER references date_times(id),
+  attendee_id INTEGER references attendees(id)
 );
 
 DROP TABLE IF EXISTS event_attendees CASCADE;
 CREATE TABLE event_attendees (
   event_id INTEGER references events(id),
-  attendee_id INTEGER references attendees(id)
-);
-
-DROP TABLE IF EXISTS attendee_date_times CASCADE;
-CREATE TABLE attendee_date_times (
-  date_time_id INTEGER references date_times(id),
   attendee_id INTEGER references attendees(id)
 );
