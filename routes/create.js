17cb8d267bb4fd778a60674b23e28abebd827743
spiritxhx <spiritxhx@gmail.com>
2019-07-27 12:15:02 -0400

    centralize the content of the createpage

--- routes/create.js
+++ routes/create.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = () => {
   router.get("/", (req, res) => {
     res.render("event-create");
+  });
+  router.post("/", (req, res) => {
+
   });
   return router;
 }
