22ae41908b9808c60e02dc9df5dea2092c8f512c
spiritxhx <spiritxhx@gmail.com>
2019-07-27 11:28:52 -0400

    add route for invite page

--- server.js
+++ server.js
@@ -36,12 +36,14 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 const eventCreate = require("./routes/create");
+const eventInvite = require("./routes/invite");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
 app.use("/create", eventCreate());
+app.use("/invite", eventInvite());
 // Note: mount other resources here, using the same pattern above
 
 
