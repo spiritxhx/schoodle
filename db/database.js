c0c5c5e07c0ff55715c53ab955c3ca3e3e627789
Nelly31 <nelly_main@hotmail.com>
2019-07-31 14:20:12 +0000

    organiser modify name fix

--- db/database.js
+++ db/database.js
@@ -41,7 +41,6 @@ const addAttendeeDetails = (attendee) => {
 
   return db.query(addAttendeeDetailsQuery, [attendee.name, attendee.email])
     .then(res => {
-      console.log("++++++++++++++", res.rows[0].id);
 
       db.query(addAvailabilityQuery, [1, res.rows[0].id])
         .catch(err => console.log(err));
@@ -54,19 +53,18 @@ const checkURL = url => {
   FULL JOIN attendees on attendees.id=owner_id WHERE event_url = $1`;
   return db.query(checkURLQuery, [url])
     .then(res => {
-      // console.log('res.rows: ', res.rows);
       return res.rows.length ? res.rows : undefined;
     })
     .catch(err => console.log(err));
 };
 
 const checkOwnerURL = url => {
-  const checkURLQuery = `SELECT title, description, start_date_time, end_date_time, name, date_times.id
+  const checkURLQuery = `SELECT title, description, start_date_time, end_date_time, name, events.id
   FROM events JOIN date_times on events.id=event_id
   FULL JOIN attendees on attendees.id=owner_id WHERE owner_url = $1`;
   return db.query(checkURLQuery, [url])
     .then(res => {
-      // console.log('res.rows: ', res.rows);
+      console.log('CHECK OWNER URL res.rows: ', res.rows);
       return res.rows.length ? res.rows : undefined;
     })
     .catch(err => console.log(err));
@@ -88,6 +86,22 @@ const fetchAttendees = url => {
     .catch(err => console.log(err));
 };
 
+const fetchAttendeesByEventId = eventid => {
+  //fetch the names and date_times of the attendees going to this event using the event id
+  const fetchQuery = `SELECT attendees.name, start_date_time, end_date_time, date_times.id
+  FROM event_attendees JOIN attendees on attendees.id = attendee_id
+  JOIN events on event_id = events.id
+  JOIN attendee_date_times on attendees.id = attendee_id
+  JOIN date_times on date_times.id = date_time_id
+  WHERE events.id = $1 AND attendees.id=attendee_date_times.attendees_id;`;
+  return db.query(fetchQuery, [eventid])
+    .then(res => {
+      // console.log('res.rows2: ', res.rows);
+      return res.rows.length ? res.rows : undefined;
+    })
+    .catch(err => console.log(err));
+};
+
 const fetchEventId = url => {
   const eventIdQuery = `SELECT id FROM events WHERE owner_url = $1;`;
   return db.query(eventIdQuery, [url])
@@ -97,10 +111,22 @@ const fetchEventId = url => {
     .catch(err => console.log(err));
 };
 
-const updateEventTitle = (title, url) => {
-  let updateQuery = `UPDATE events SET title = $1 WHERE owner_url = $2;`;
-  return db.query(updateQuery, [title, url])
+const updateEventTitle = (title, eventid) => {
+  let updateQuery = `UPDATE events SET title = $1 WHERE id = $2;`;
+  return db.query(updateQuery, [title, eventid])
     .then(res => {
+      return res.rows;
+    })
+    .catch(err => console.log(err));
+};
+
+const fetchEventInfo = eventid => {
+  const fetchEventInfoQuery = `SELECT title, description, owner_url, start_date_time, end_date_time, name, date_times.id, events.id
+  FROM events JOIN date_times on events.id=event_id
+  FULL JOIN attendees on attendees.id=owner_id WHERE events.id = $1`;
+  return db.query(fetchEventInfoQuery, [eventid])
+    .then(res => {
+      // console.log(res);
       return res.rows;
     })
     .catch(err => console.log(err));
@@ -114,3 +140,5 @@ exports.fetchAttendees = fetchAttendees;
 exports.checkOwnerURL = checkOwnerURL;
 exports.updateEventTitle = updateEventTitle;
 exports.fetchEventId = fetchEventId;
+exports.fetchEventInfo = fetchEventInfo;
+exports.fetchAttendeesByEventId = fetchAttendeesByEventId;
